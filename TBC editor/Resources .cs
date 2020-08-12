60f17ac2c967b6b0a33b0cece7cd383377f8b9fb
69EHOTUK96 <raccoon.twitch@gmail.com>
2020-08-13 00:17:58 +0500

    some fixes

--- TBC editor/Resources .cs
+++ TBC editor/Resources .cs
@@ -17,11 +17,11 @@ namespace TBC_editor
         public List<Bitmap> Emotions = new List<Bitmap>();
         public List<Bitmap> Clothes = new List<Bitmap>();
         public Person(params string[] ps)
-            {
+        {
             Name = ps[0];
             Tag = ps[1];
             Color = ps[2];
-            }
+        }
     }
 
     public class Resources
@@ -30,9 +30,9 @@ namespace TBC_editor
         public Resources()
         {
             var path = Directory.GetCurrentDirectory() + "\\persons.txt";
-            using (StreamReader sr = new StreamReader(path))
+            using (var sr = new StreamReader(path))
             {
-                while(!sr.EndOfStream)
+                while (!sr.EndOfStream)
                 {
                     var vars = sr.ReadLine().Split(' ');
                     if (vars.Length != 3)
@@ -44,27 +44,26 @@ namespace TBC_editor
             path = Directory.GetCurrentDirectory() + "\\images\\sprites\\";
             DirectoryInfo dir = new DirectoryInfo(path);
             var files = dir.GetFiles();
-            foreach(var person in persons)
+            foreach (var person in persons)
             {
-                foreach(var file in files)
+                foreach (var file in files)
                 {
-                    if(file.Name.Substring(0,person.Tag.Length+1) == person.Tag+"_")
+                    if (file.Name.Substring(0, person.Tag.Length + 1) != person.Tag + "_")
+                        continue;
+                    var image = new Bitmap(path + "\\" + file.Name);
+                    var tag = String.Join(" ", file.Name.Split('.')[0].Split('_').ToList());
+                    image.Tag = tag;
+                    if (file.Name.Contains("telo"))
                     {
-                        var image = new Bitmap(path + "\\" + file.Name);
-                        var tag = String.Join(" ", file.Name.Split('.')[0].Split('_').ToList());
-                        image.Tag = tag;
-                        if (file.Name.Contains("telo"))
-                        {
-                            person.Bodies.Add(image);
-                            continue;
-                        }
-                        if (file.Name.Contains("pion") || file.Name.Contains("ulica") || file.Name.Contains("sport") || file.Name.Contains("swim") || file.Name.Contains("dress"))
-                        {
-                            person.Clothes.Add(image);
-                            continue;
-                        }
-                        person.Emotions.Add(image);
+                        person.Bodies.Add(image);
+                        continue;
                     }
+                    if (file.Name.Contains("pion") || file.Name.Contains("ulica") || file.Name.Contains("sport") || file.Name.Contains("swim") || file.Name.Contains("dress"))
+                    {
+                        person.Clothes.Add(image);
+                        continue;
+                    }
+                    person.Emotions.Add(image);
                 }
             }
             var a = persons;
